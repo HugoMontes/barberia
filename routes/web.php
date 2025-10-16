6eda52bb6c3fd2edbc066d8c09fb48bde77ff3e4
hmontes <hugomontes927@gmail.com>
2025-10-16 19:54:26 -0400

    Login: Instalar breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\BarberoController;
 use App\Http\Controllers\Admin\ClienteController;
 use App\Http\Controllers\Admin\FacturaController;
@@ -11,9 +12,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/plantilla', function () {
-    return view('admin.layouts.main');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__ . '/auth.php';
 
 Route::resource('cliente', ClienteController::class);
 Route::resource('barbero', BarberoController::class);
